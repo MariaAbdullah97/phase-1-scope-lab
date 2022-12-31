eb09cd191fda13ec902fee684a7e455ad48685c8
MariaAbdullah97 <meroo3066@gmail.com>
2022-12-31 14:31:04 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName ="bob";
+var bestCustomer;
+const leastFavoriteCustomer="ali";
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+    return bestCustomer="not bob";
+
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer="maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer="Assignment to constant variable.";
+}
+
+
+const x=upperCaseCustomerName();
+console.log(x);
+  
